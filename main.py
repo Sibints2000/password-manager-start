70bdd83935ca45b1c92557d9face4c2f8dd67030
Sibints2000 <sibints2000@gmail.com>
2021-08-24 21:56:38 +0530

    Twenty Eighth Commit

--- main.py
+++ main.py
@@ -25,17 +25,17 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=42)
 website_entry.grid(column=1, row=1, columnspan=2)
-email_entry = Entry(width=35)
+email_entry = Entry(width=42)
 email_entry.grid(column=1, row=2, columnspan=2)
-password_entry = Entry(width=21)
+password_entry = Entry(width=24)
 password_entry.grid(column=1, row=3)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=35)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
