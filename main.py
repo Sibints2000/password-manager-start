9c254ce12098cc20b91392bc627440ea97105edd
Sibints2000 <sibints2000@gmail.com>
2021-08-24 21:44:47 +0530

    Twenty Second Commit

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 # Labels
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-email_label = Label(text="Email/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
-password_label = Label(text="Password")
+password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entries
