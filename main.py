0110ef12d8c28631889d2eb13d129316f2513ecb
Sibints2000 <sibints2000@gmail.com>
2021-08-24 22:57:18 +0530

    Fifty Fifth Commit

--- main.py
+++ main.py
@@ -18,11 +18,6 @@ nr_numbers = random.randint(2, 4)
 
 password_letters = [random.choice(letters) for _ in range(nr_letters)]
 
-for char in range(nr_letters):
-    password_list.append(random.choice(letters))
-
-for char in range(nr_symbols):
-    password_list += random.choice(symbols)
 
 for char in range(nr_numbers):
     password_list += random.choice(numbers)
