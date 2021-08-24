4ea5fcd379202f6c4ce53ecf2228efeebf4d0ca8
Sibints2000 <sibints2000@gmail.com>
2021-08-24 20:12:22 +0530

    Ninth Commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ window.title("Password Manager")
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-
+canvas.pack()
 
 
 window.mainloop()
